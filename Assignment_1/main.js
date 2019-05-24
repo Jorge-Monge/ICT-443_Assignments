b1563dd597346b0010a2919427bc5dd71c56ef2e
Jordimonk <jordimonk@yahoo.com>
2019-05-23 23:02:36 -0600

    Setting things up

--- Assignment_1/main.js
+++ Assignment_1/main.js
@@ -2,7 +2,7 @@
 // Global variables
 var itemsArray = [];
 var item = "";
-
+/*
 while (item !== "x") {
     item = prompt("Enter grocery item");
     if (item === "x") break;
@@ -15,3 +15,4 @@ for (var i = 0; i<itemsArray.length; i++) {
     console.log("Item number " + (i+1) + " is " + itemsArray[i]);
     shoppingListDiv.innerHTML += "Item number " + (i+1) + " is " + itemsArray[i] + "<br>";
 }
+*/
